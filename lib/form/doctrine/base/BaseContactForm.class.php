35feeb9b50caaf0e2050ed0da2e9142f77c60d5b
m.mure <m.mure@55463ab2-4f42-40d6-a48e-481de4af6215>
2011-03-01 14:40:00 +0000

    correction de lien dans les formulaires du module annuaire
    git-svn-id: https://svn.iariss.fr/erp@61 55463ab2-4f42-40d6-a48e-481de4af6215

--- lib/form/doctrine/base/BaseContactForm.class.php
+++ lib/form/doctrine/base/BaseContactForm.class.php
@@ -8,7 +8,7 @@
  * @package    Annuaire
  * @subpackage form
  * @author     Michael Muré
- * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
  */
 abstract class BaseContactForm extends BaseFormDoctrine
 {
@@ -26,7 +26,7 @@ abstract class BaseContactForm extends BaseFormDoctrine
     ));
 
     $this->setValidators(array(
-      'id'              => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'id', 'required' => false)),
+      'id'              => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
       'date'            => new sfValidatorDate(),
       'commentaire'     => new sfValidatorString(array('max_length' => 4000, 'required' => false)),
       'prospect_id'     => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Prospect'))),
