2f293b87a58475d8b3c7a9b1b7ff5afdeb92b99c
m.mure <m.mure@55463ab2-4f42-40d6-a48e-481de4af6215>
2011-03-01 18:45:49 +0000

    affichage du prospect dans la liste des contacts
    git-svn-id: https://svn.iariss.fr/erp@72 55463ab2-4f42-40d6-a48e-481de4af6215

--- apps/frontend/modules/contact/templates/_list.php
+++ apps/frontend/modules/contact/templates/_list.php
@@ -6,6 +6,7 @@
       <th></th>
       <th>Date</th>
       <th>Membre</th>
+      <th>Prospect</th>
       <th>Commentaire</th>
     </tr>
   </thead>
@@ -15,6 +16,7 @@
       <td><img src="/images/<?php echo $contact->getTypeContact()->getLogo() ?>-mini.png" width="16" height="16"/></td>
       <td><a href="<?php echo url_for('@contact?action=show&id='.$contact->getId()) ?>"><?php echo format_date($contact->getDate()) ?></a></td>
       <td><?php echo ($contact->getMembreId()) ? $contact->getMembre() : "" ?></td>
+      <td><?php echo ($contact->getProspectId()) ? $contact->getProspect() : "" ?></td>
       <td><?php echo $contact->getCommentaire() ?></td>
     </tr>
     <?php endforeach; ?>
