b4c144057ef7b8db7342d140d8b9f399481e818e
m.mure <m.mure@55463ab2-4f42-40d6-a48e-481de4af6215>
2011-03-02 08:22:01 +0000

    un coup de refactoring
    git-svn-id: https://svn.iariss.fr/erp@83 55463ab2-4f42-40d6-a48e-481de4af6215

--- apps/frontend/modules/contact/templates/indexSuccess.php
+++ apps/frontend/modules/contact/templates/indexSuccess.php
@@ -10,20 +10,18 @@
 <?php echo link_to('Afficher uniquement les appels', '@contact?action=index&filter=appel') ?><br />
 <?php echo link_to('Ne pas filtrer', '@contact?action=index') ?><br />
 
-<?php switch($filter)
-      {
-        case 'index': echo link_to('Ajouter un contact', '@contact?action=new'); break;
-        case 'email': echo link_to('Ajouter un email', '@contact?action=new&type=email'); break;
-        case 'appel': echo link_to('Ajouter un appel', '@contact?action=new&type=appel'); break;
-      }
-?>
-
+<?php echoAddLinks($filter) ?>
 <?php include_partial('contact/list', array('contacts' => $contacts)) ?>
+<?php echoAddLinks($filter) ?>
 
-<?php switch($filter)
-      {
-        case 'index': echo link_to('Ajouter un contact', '@contact?action=new'); break;
-        case 'email': echo link_to('Ajouter un email', '@contact?action=new&type=email'); break;
-        case 'appel': echo link_to('Ajouter un appel', '@contact?action=new&type=appel'); break;
-      }
+<?php
+function echoAddLinks($filter)
+{
+  switch($filter)
+  {
+    case 'index': echo link_to('Ajouter un contact', '@contact?action=new'); break;
+    case 'email': echo link_to('Ajouter un email', '@contact?action=new&type=email'); break;
+    case 'appel': echo link_to('Ajouter un appel', '@contact?action=new&type=appel'); break;
+  }
+}
 ?>
