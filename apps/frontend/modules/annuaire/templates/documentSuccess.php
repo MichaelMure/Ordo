b521dfa75a512e9560aad95dbc5bf2a7a50a5f96
Michael Muré <batolettre@gmail.com>
2011-11-12 18:55:35 +0100

    fix wrong regression in document layout

--- apps/frontend/modules/annuaire/templates/documentSuccess.php
+++ apps/frontend/modules/annuaire/templates/documentSuccess.php
@@ -20,8 +20,8 @@
                 else echo $membre; ?></td>
       <td><?php echo $membre->getTelMobile() ?></td>
       <td class='<?php echo $membre->getcarteId() ? 'vert' : 'rouge' ?>'><?php echo $membre->getcarteId() ? 'oui' : 'non' ?></td>
-      <td class='<?php echo $membre->getQuittance() ? 'vert' : 'rouge' ?>'><?php echo $membre->getQuittance() ? 'oui' : 'non' ?></td>
-      <td class='<?php echo $membre->getCotisation() ? 'vert' : 'rouge' ?>'><?php echo $membre->getCotisation() ? 'oui' : 'non' ?></td>
+      <td class='<?php echo $membre->getCurrentQuittance() ? 'vert' : 'rouge' ?>'><?php echo $membre->getCurrentQuittance() ? 'oui' : 'non' ?></td>
+      <td class='<?php echo $membre->getCurrentCotisation() ? 'vert' : 'rouge' ?>'><?php echo $membre->getCurrentCotisation() ? 'oui' : 'non' ?></td>
       <td class='<?php echo $membre->getJustDomicile() ? 'vert' : 'rouge' ?>'><?php echo $membre->getJustDomicile() ? 'oui' : 'non' ?></td>
       <td class='<?php echo $membre->getReglementInterieur() ? 'vert' : 'rouge' ?>'><?php echo $membre->getReglementInterieur() ? 'oui' : 'non' ?></td>
       <td class='<?php echo $membre->getConventionEtudiant() ? 'vert' : 'rouge' ?>'><?php echo $membre->getConventionEtudiant() ? 'oui' : 'non' ?></td>
